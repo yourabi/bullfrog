fec0bcdac4f251f36332a6fe33475df1ee57b81b
Yousef Ourabi <yourabi@sunspot.local>
2009-11-16 22:36:53 -0800

    adding setup.py and moving things around to accommodate friendly packaging

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License. 
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+Version = 0.1
+setup ( name='bullfrog',
+        version = Version,
+        description="Python framework to fetch content in paralle",
+        author = "Yousef Ourabi",
+        author_email = "yourabi@gmail.com",
+        url = "http://github.com/yourabi/bullfrog",
+        packages = ['bullfrog', 'bullfrog.plugins'],
+        license = 'BSD',
+        platforms = 'Posix; MacOS X;',
+        classifiers = ['Intended Audience :: Developers',
+                       'License :: OSI Approved :: BSD License',
+                       'Topic :: Internet :: WWW/HTTP',
+                       'Topic :: Software Development :: Libraries :: Application Frameworks',
+                       'Topic :: Software Development :: Libraries :: Python Modules',
+                      ],
+     )
